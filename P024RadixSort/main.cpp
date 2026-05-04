cfc7c2572b81f94565a38cd39b95f4e2dae9d9b5
CrimsonBlood45 <asaavedrar2400@alumno.ipn.mx>
2026-05-04 13:40:10 -0600

    Added P024RadixSort Code

--- P024RadixSort/main.cpp
+++ P024RadixSort/main.cpp
@@ -1,46 +1,223 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <chrono>
 
 using namespace std;
 
-int buscarTexto(const char* texto, const char* subtexto) {
-    int i = 0, j = 0;
-    // Ciclo principal para recorrer el texto
-    while (texto[i] != '\0') {
-        // Si se encuentra una coincidencia inicial, comparar subtexto completo
-        while (texto[i + j] != '\0' && subtexto[j] != '\0' && texto[i + j] ==
-            subtexto[j]) {
-            j++;
-        }
-        // Si se alcanzó el fin del subtexto, es una coincidencia completa
-        if (subtexto[j] == '\0') {
-            return i; // Retorna la posición de inicio de la coincidencia
-        }
-        // Reiniciar j y avanzar al siguiente carácter
-        j = 0;
-        i++;
+#define readErr       "\t[!]Error: Fallo al leer. El archivo ya ha sido leído\n"
+#define sortErr       "\t[!]Error: Fallo al ordenar. El archivo ya ha sido ordenado\n"
+#define sortReadErr   "\t[!]Error: Fallo al ordenar. No se ha leido ningún archivo\n"
+#define sortRecErr    "\t[!]Error: Fallo al ordenar. El archivo ya fue grabado\n"
+#define recordWarn    "\t[!]Advertencia: Se sobreescribirá el archivo\n"
+#define recordReadErr "\t[!]Error: Fallo al grabar. No se han leido datos\n"
+#define showReadErr   "\t[!]Error: Fallo al mostrar. No se ha leído ningún archivo\n"
+#define showRecErr    "\t[!]Error: Fallo al mostrar. Archivo ya fue grabado\n"
+#define exitReadErr   "\t[!]Error: Fallo al salir. No se ha leído ningún archivo\n"
+#define exitRecErr    "\t[!]Error: Fallo al salir. No se ha grabado el archivo\n"
+
+void leerArchivo        (struct arreglos *arreglo);
+bool tieneExtensionTxt  (string nombre);
+void sort               (struct arreglos *arreglo);
+void displayArray       (struct arreglos *arreglo);
+void recordFile         (struct arreglos *arreglo);
+bool validAction        (int opt, struct arreglos *arreglo);
+int obtenerMaximo       (int arr[], int tam);
+void countingSort       (int arr[], int n, int exp);
+void radixSort          (int arr[], int n);
+
+struct arreglos{
+    int size = 0;
+    int* elems = nullptr;
+    bool read = 0, sorted = 0, recorded = 0;
+    bool isEmpty(){return elems == nullptr || size == 0; }
+    bool check(bool condicion, const char* mensaje) {
+        if (condicion) cout << mensaje;
+        return !condicion;
     }
-    // Si no se encontró el subtexto, retornar -1
-    return -1;
+};
+
+int main(){
+    int opt;
+    struct arreglos arreglo1;
+    
+    do{
+        cout << "\t\t\tMenú Ordenamiento por Selección\n" <<
+        "Elija una Opción (Ingresar Número): \n" << 
+        "1. Leer Archivo\n" <<
+        "2. Ordenar\n" << 
+        "3. Grabar Archivo\n" <<
+        "4. Mostrar Arreglo\n" << 
+        "5. Salir\n" << 
+        " > ";
+        cin >> opt;
+        if(!validAction(opt, &arreglo1)) continue;
+        switch(opt){
+            case 1: leerArchivo(&arreglo1); break;
+            case 2: sort(&arreglo1); break;
+            case 3: recordFile(&arreglo1); break;
+            case 4: displayArray(&arreglo1); break;
+            case 5: return 0;
+        }
+    }while(true);
 }
-int main() {
-    // Entradas del usuario
-    char texto[100], subtexto[50];
 
-    cout << "Ingrese el texto principal: ";
-    cin.getline(texto, 100);
+void leerArchivo(struct arreglos *arreglo){
+ 
+    string nombreArchivo;
 
-    cout << "Ingrese el subtexto a buscar: ";
-    cin.getline(subtexto, 50);
-    // Llamada a la función de búsqueda
-    int posicion = buscarTexto(texto, subtexto);
-    // Resultado
-    if (posicion != -1) {
-        cout << "El subtexto se encontró en la posición " <<
-        posicion << " del texto principal." << endl;
-    } 
-    else{
-        cout << "El subtexto no se encontró en el texto principal." << endl;
+    do{
+        delete[] arreglo->elems;
+        arreglo->elems = nullptr;
+
+        cout << "Ingrese el nombre del archivo sin extensión: ";
+        cin  >> nombreArchivo;
+
+        if(tieneExtensionTxt(nombreArchivo)){
+            cout << "  [!] No incluyas la extensión. " << "Escribe solo el nombre: \n";
+            arreglo->elems = nullptr;
+            continue;
+        }
+
+        nombreArchivo += ".txt";
+
+        ifstream archivo(nombreArchivo);
+        if (!archivo) {
+            cout << "  [!] No se pudo abrir: " << nombreArchivo << "\n";
+            arreglo->elems = nullptr;
+            continue;
+        }
+    
+        int temp;
+        while (archivo >> temp) {
+            arreglo->size++;
+        }
+    
+        if (arreglo->size == 0) {
+            cout << "  [!] El archivo \"" << nombreArchivo << "\" está vacío o no contiene enteros.\n";
+            arreglo->elems = nullptr;
+            continue;
+        }
+    
+        arreglo->elems = new int[arreglo->size];
+    
+        archivo.clear();
+        archivo.seekg(0, ios::beg);
+    
+        for (int i = 0; i < arreglo->size; i++) {
+            archivo >> arreglo->elems[i];
+        }
+    
+        continue;
+    }while(arreglo->isEmpty());
+    arreglo->read = true;
+    cout << "\n  Archivo leído correctamente. " << arreglo->size << " entero(s) encontrado(s):\n\n";
+    return;
+ }
+bool tieneExtensionTxt(string nombre){
+    string ext = ".txt";
+    if (nombre.size() < ext.size()) return false;
+ 
+    string sufijo = nombre.substr(nombre.size() - ext.size());
+    for (char& c : sufijo) c = tolower(c);
+ 
+    return sufijo == ext;
+}
+void sort(struct arreglos *arreglo){
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // Llamada a QuickSort
+    radixSort(arreglo->elems, arreglo->size);
+
+    arreglo->sorted = true;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    cout << "\tArreglo Ordenado en " << duration.count()/1000000.0 << " segundos\n\n";
+}
+void displayArray(struct arreglos *arreglo){
+    cout << "Arreglo Actual: ";
+    if(arreglo->isEmpty()){cout << "(Empty)\n\n";}
+    for(int i = 0; i < arreglo->size; i++){
+        cout << arreglo->elems[i] << " ";
+    }
+    cout << "\n\n";
+}
+void recordFile(struct arreglos *arreglo){
+    string nombre;
+    do{
+        cout << "Ingrese el nombre del archivo para guardar (sin extensión): ";
+        cin >> ws;
+        getline(cin, nombre);
+        if(tieneExtensionTxt(nombre)){
+            cout << "  [!] No incluyas la extensión. " << "Escribe solo el nombre: \n";
+            continue;
+        }
+        nombre += ".txt";
+        ofstream archivo(nombre);
+        if(!archivo){
+            cout << "No se pudo crear el archivo\n";
+            return;
+        }
+        for(int i = 0; i < arreglo->size; i++){
+            archivo << arreglo->elems[i] << " ";
+        }
+        archivo.close();
+        arreglo->recorded = true;
+        cout << "Archivo Guardado Exitosamente como: " << nombre << endl;
+    }while(!arreglo->recorded);
+}
+bool validAction(int opt, arreglos *arreglo) {
+    switch (opt) {
+        case 1: return arreglo->check(arreglo->read,       readErr);
+        case 2: return arreglo->check(!arreglo->read,      sortReadErr)
+                    && arreglo->check(arreglo->sorted,     sortErr)
+                    && arreglo->check(arreglo->recorded,   sortRecErr );
+        case 3: if(arreglo->recorded) cout << recordWarn;
+                return arreglo->check(!arreglo->read,      recordReadErr);
+        case 4: return arreglo->check(!arreglo->read,      showReadErr)
+                    && arreglo->check(arreglo->recorded,   showRecErr);
+        case 5: return arreglo->check(!arreglo->read,      exitReadErr)
+                    && arreglo->check(!arreglo->recorded,  exitRecErr);
+        default: cout << "Opción Inválida. Intente de Nuevo... \n"; return false;
+    }
+}
+int obtenerMaximo(int arr[], int tam) {
+    int maximo = arr[0];
+    for (int i = 1; i < tam; i++) {
+        if (arr[i] > maximo)
+            maximo = arr[i];
+    }
+    return maximo;
+}
+void countingSort(int arr[], int n, int exp) {
+    int* salida = new int[n]; 
+    int conteo[10] = {0};
+
+    // Contar ocurrencias de cada dígito (0-9)
+    for (int i = 0; i < n; i++)
+        conteo[(arr[i] / exp) % 10]++;
+
+    // Cambiar conteo[i] para que contenga la posición real en salida
+    for (int i = 1; i < 10; i++)
+        conteo[i] += conteo[i - 1];
+
+    // Construir el arreglo de salida de forma estable[cite: 7]
+    for (int i = n - 1; i >= 0; i--) {
+        int idx = (arr[i] / exp) % 10;
+        salida[conteo[idx] - 1] = arr[i];
+        conteo[idx]--;
     }
 
-    return 0;
+    // Copiar al arreglo original[cite: 7]
+    for (int i = 0; i < n; i++)
+        arr[i] = salida[i];
+
+    delete[] salida;
+}
+void radixSort(int arr[], int n) {
+    int m = obtenerMaximo(arr, n);
+    // Aplicar counting sort para cada posición decimal: 1, 10, 100...[cite: 7]
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countingSort(arr, n, exp);
 }
